77f94afcbd7fc0c294afc9042ec86f20ba3ca9e1
yuting0926 <80568196+yuting0926@users.noreply.github.com>
2021-05-08 15:20:20 +0800

    Add files via upload

--- untitled.py
+++ untitled.py
@@ -1,5 +1,21 @@
-import requests
-url = 'https://www.youtube.com/'
+import time
+from selenium import webdriver
 
-data = requests.get(url)
-print(data.text)
+chrome = webdriver.Chrome()
+chrome.get('http://www.google.com')
+inputBar = chrome.find_element_by_class_name('gLFyf.gsfi')
+inputBar.send_keys('猿創力')
+time.sleep(1)
+
+
+button = chrome.find_element_by_class_name('gNO89b')
+button.click()
+time.sleep(1)
+
+button = chrome.find_element_by_class_name('LC20lb.DKV0Md')
+time.sleep(1)
+button.click()
+
+
+chrome.maximize_window()
+chrome.get_screenshot_as_file('123.png')
